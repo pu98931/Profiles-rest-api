02cee64208fd15877ff552911f5bdf2a60f559dd
Mark <pu98931@gmail.com>
2021-08-11 23:29:26 -0400

    Enable Django admin user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -36,9 +36,8 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
-    # is_active = models.Booleanfield(default=True)
-    # is_staff = models.Booleanfield(default=True)
+    is_staff = models.BooleanField(default=False)
+
 
     objects = UserProfileManager()
 
